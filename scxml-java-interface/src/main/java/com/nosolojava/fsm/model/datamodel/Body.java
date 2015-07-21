aff893afafe820e47a29ecf1e7c347de8a4beedb
Carlos Verdes <cverdes@gmail.com>
2015-07-21 13:04:56 +0100

    updates for maven repo

--- scxml-java-interface/src/main/java/com/nosolojava/fsm/model/datamodel/Body.java
+++ scxml-java-interface/src/main/java/com/nosolojava/fsm/model/datamodel/Body.java
@@ -11,7 +11,7 @@ import com.nosolojava.fsm.runtime.Context;
  * This class is used when an elemen can have both &lt;param&gt; and &lt;content&gt; children. When in evaluated it
  * retrieves the content or the params depending on the configuration (can't be both params and content in the same
  * element).
- * <p> This will be used in {@link DoneData}, {@link Send} and {@link Invoke}.
+ * <p> This will be used in {@link DoneData}, {@link Send} and {@link Invoke}.</p>
  * 
  * @author Carlos Verdes
  * 
@@ -21,7 +21,7 @@ public interface Body {
 	Set<String> getLocations();
 	
 	/**
-	 * @return true if the method evaluateBody will return a Map<String,Serializable>.
+	 * @return true if the method evaluateBody will return a {@code Map<String,Serializable>}.
 	 */
 	boolean isParamsBody();
 	
